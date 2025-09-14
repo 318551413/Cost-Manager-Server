3e3ea45072679085a530b0867d7d7b420630406b
318551413 <135502466+318551413@users.noreply.github.com>
2025-09-14 14:29:18 +0300

    Update cost.js

--- models/cost.js
+++ models/cost.js
@@ -1,19 +1,22 @@
-// models/cost.js (updated)
-
+// models/cost.js
+// This file defines the Mongoose schema and model for 'Cost' documents.
 const mongoose = require('mongoose');
+
 // Define the Cost schema
 const costSchema = new mongoose.Schema({
     description: { type: String, required: true },
+    // 'category' field: A string that is required. The value must be one of the specified options.
     category: {
         type: String,
         required: true,
         enum: ['food', 'health', 'housing', 'sports', 'education']
     },
+    // 'userid' field: A number that links the cost to a specific user. It is required.
     userid: { type: Number, required: true },
     sum: { type: Number, required: true },
     date: { type: Date, default: Date.now } // Added date field
 });
-
+// This model provides an interface for interacting with the 'costs' collection in the database.
 const Cost = mongoose.model('Cost', costSchema);
 
 module.exports = Cost;
